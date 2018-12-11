28d0e0156560f777796e0b30739567e3c5920857
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2018-12-11 21:51:59 +0700

    Add files via upload

--- Classify.py
+++ Classify.py
@@ -1,31 +1,21 @@
 ''' Classify '''
-def main(dic, result, old_year, old_fac):
+def main():
     ''' for '''
+    dic = dict()
     while True:
-        cha = input()
-        if cha == 'END':
+        idem = input()
+        if idem == "END":
             break
+        if idem[:4] not in dic:
+            dic[idem[:4]] = 1
         else:
-            if int(cha[0:2]) in dic:
-                dic[int(cha[0:2])].append(int(cha[2:4]))
-            else:
-                dic[int(cha[0:2])] = [int(cha[2:4])]
-    lis = sorted(list(dic))
-    print(dic)
-    print(lis)
-    for i in lis: # function print
-        dic[i] = sorted(dic[i])
-        for j in range(len(dic[i])):
-            if old_fac != dic[i][j] or i != old_year:
-                if str(i) not in result:
-                    result.extend([str(i), [str(dic[i][j])+' '+str(dic[i].count(dic[i][j]))]])
-                else:
-                    result.extend(['--', [str(dic[i][j])+' '+str(dic[i].count(dic[i][j]))]])
-                old_fac = dic[i][j]
-                old_year = i
-                # print(result)
-        for j in range(0, len(result), 2):
-            print(result[j], *result[j+1])
-        result = []
+            dic[idem[:4]] += 1
+    year = []
+    for i in sorted(dic):
+        if i[:2] != year:
+            print(i[:2], int(i[2:]), dic[i])
+        else:
+            print('--', int(i[2:]), dic[i])
+        year = i[:2]
 
-main({}, [], '', '')
+main()
